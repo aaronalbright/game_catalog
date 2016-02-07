eaf5ea557801e690235ddf514b458bd6edb877e1
aaronja38 <Aaron@host-10-30-248-136.nomadic.ufl.edu>
2016-02-06 19:43:59 -0500

    Installed Bootstrap

--- inventory_list.php
+++ inventory_list.php
@@ -9,6 +9,7 @@
     <meta charset="utf-8">
     <meta name=viewport content="width=device-width, initial-scale=1">
     <title> Games Galor Inventory </title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/table.css">
 </head>
